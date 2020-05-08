a53af830bb675a4bb295d27a8d55aa224e8a2689
kangana512 <kangana512@gmail.com>
2020-05-08 16:36:24 +0700

    😇Send Message Client -> Server

--- greet/greet_client/client.go
+++ greet/greet_client/client.go
@@ -1,20 +1,39 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 
-	"github.com/golang/protobuf/protoc-gen-go/grpc"
 	"github.com/kangana1024/grpc-go/greet/greetpb"
+	"google.golang.org/grpc"
 )
 
 func main() {
 	fmt.Println("Hello I'm a client")
 
-	conn,err := grpc.Dial("localhost:50051",grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 
 	if err != nil {
-		log.Fatalf("could not connect: %v",err)
+		log.Fatalf("could not connect: %v", err)
 	}
-
+	defer conn.Close()
 	c := greetpb.NewGreetServiceClient(conn)
+
+	doUnary(c)
+}
+
+func doUnary(c greetpb.GreetServiceClient) {
+	fmt.Println("Starting to do a unary RPC...")
+	req := &greetpb.GreetRequest{
+		Greeting: &greetpb.Greeting{
+			FirstName: "KANGANA",
+			LastName:  "SONDEE",
+		},
+	}
+	res, err := c.Greet(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while call greet rpc: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
 }
